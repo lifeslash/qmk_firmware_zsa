92a4469347fe2af06b0ab496c8d8d715181ab4ff
again <lifeslash.mail@gmail.com>
2024-02-12 05:35:48 +0000

    update

--- keyboards/voyager/keymaps/lifeslash_mac_ja/keymap.c
+++ keyboards/voyager/keymaps/lifeslash_mac_ja/keymap.c
@@ -41,14 +41,12 @@ enum tap_dance_codes {
   TD_N_RBRC,  // KC_N, KC_RBRC
 };
 
-// FIXME: tap dance feature does not worked!
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                                           KC_7,           KC_8,           KC_9,           KC_0,           KC_GRV,         KC_WAKE,       
-    MT(MOD_LCTL,KC_TAB),LT(1,KC_Q), KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           LT(1,KC_P),     KC_DEL,      
-    CW_TOGG,        LT(2,KC_A),     KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(2,KC_SCLN),  MT(MOD_RCTL,KC_APP),
-    SC_LSPO,        MT(MOD_LGUI,KC_Z),MT(MOD_LALT,KC_X),KC_C,       KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       MT(MOD_RALT,KC_DOT),MT(MOD_RGUI,KC_SLSH),SC_RSPC,       
+    MT(MOD_LCTL,KC_TAB),LT(1,KC_Q), KC_W,           KC_E,           KC_R,           TD(TD_T_QUOT),                                  TD(TD_Y_BSLS),  KC_U,           KC_I,           KC_O,           LT(1,KC_P),     KC_DEL,      
+    CW_TOGG,        LT(2,KC_A),     KC_S,           KC_D,           KC_F,           TD(TD_G_EQL),                                   TD(TD_H_MINS),  KC_J,           KC_K,           KC_L,           LT(2,KC_SCLN),  MT(MOD_RCTL,KC_APP),
+    SC_LSPO,        MT(MOD_LGUI,KC_Z),MT(MOD_LALT,KC_X),KC_C,       KC_V,           TD(TD_B_LBRC),                                  TD(TD_N_RBRC),  KC_M,           KC_COMMA,       MT(MOD_RALT,KC_DOT),MT(MOD_RGUI,KC_SLSH),SC_RSPC,       
                                                                     KC_SPC,         KC_BSPC,                                        HANZEN,         KC_ENT
   ),
   [1] = LAYOUT_voyager(
@@ -76,12 +74,20 @@ combo_t key_combos[COMBO_COUNT] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case HANZEN:
-      if (is_hanzen) {
-        is_hanzen = false;
-        register_code(KC_LNG1);
+      if (record->event.pressed) {
+        if (is_hanzen) {
+          register_code(KC_LNG1);
+        } else {
+          register_code(KC_LNG2);
+        }
       } else {
-        is_hanzen = true;
-        register_code(KC_LNG2);
+        if (is_hanzen) {
+          is_hanzen = false;
+          unregister_code(KC_LNG1);
+        } else {
+          is_hanzen = true;
+          unregister_code(KC_LNG2);
+        }
       }
       return false;
     case RGB_SLD:
