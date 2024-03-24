754a95da56d1dc9f6d51d14826d64a1bc793b114
again <lifeslash.mail@gmail.com>
2024-03-24 05:05:39 +0000

    update command

--- lifeslash_compile.sh
+++ lifeslash_compile.sh
@@ -1,3 +1,8 @@
+#git remote -v
+#git fetch upstream firmware23
+#git pull upstream firmware23:firmware23
+#git push origin firmware23
+
 macBin="voyager_lifeslash_mac_ja.bin"
 if [ -e $macBin ]; then
     rm $macBin
