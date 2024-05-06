a8f0cd146dcf4733e932322f28e2ca23c52517b2
again <lifeslash.mail@gmail.com>
2024-05-06 06:27:57 +0000

    update

--- keyboards/voyager/keymaps/lifeslash_mac_ja/keymap.c
+++ keyboards/voyager/keymaps/lifeslash_mac_ja/keymap.c
@@ -37,7 +37,7 @@ uint16_t alt_tab_timer = 0;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     TD(TD_1_LEFT),  KC_2,           KC_3,           KC_4,           KC_5,           KC_6,                                           KC_7,           KC_8,           KC_9,           KC_0,           CMDTAB,         TD(TD_GRV_RGHT),       
-    KC_TAB,         LT(1,KC_Q),     KC_W,           KC_E,           KC_R,           TD(TD_T_QUOT),                                  TD(TD_Y_BSLS),  KC_U,           KC_I,           KC_O,           LT(1,KC_P),     KC_DEL,
+    KC_TAB,         LT(1,KC_Q),     KC_W,           TD(TD_E_LCBR),  TD(TD_R_LBRC),  TD(TD_T_QUOT),                                  TD(TD_Y_BSLS),  TD(TD_U_RBRC),  TD(TD_I_RCBR),  KC_O,           LT(1,KC_P),     KC_DEL,
     MT(MOD_LCTL,KC_CAPS),LT(2,KC_A),KC_S,           KC_D,           KC_F,           TD(TD_G_EQL),                                   TD(TD_H_MINS),  KC_J,           KC_K,           KC_L,           LT(2,KC_SCLN),  MT(MOD_LCTL,KC_MCTL),
     SC_LSPO,        MT(MOD_LGUI,KC_Z),MT(MOD_LALT,KC_X),KC_C,       KC_V,           TD(TD_B_PLUS),                                  TD(TD_N_UNDS),  KC_M,           KC_COMMA,       MT(MOD_RALT,KC_DOT),MT(MOD_RGUI,KC_SLSH),SC_RSPC,
                                                                     KC_SPC,         KC_BSPC,                                        HANZEN,         KC_ENT
--- keyboards/voyager/keymaps/lifeslash_mac_ja/tap_dance/tap_dance_action.c
+++ keyboards/voyager/keymaps/lifeslash_mac_ja/tap_dance/tap_dance_action.c
@@ -1,93 +1,147 @@
 // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 // tap dance function enum
 // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP = 1,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP, // Send two single taps
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD
+} td_state_t;
+
 typedef struct {
     bool is_press_action;
-    uint8_t step;
-} tap;
-
-enum {
-    SINGLE_TAP = 1,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    DOUBLE_SINGLE_TAP,
-    MORE_TAPS
-};
+    td_state_t state;
+} td_tap_t;
 
 // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 // define tap_dance custom keycodes
 // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 enum tap_dance_codes {
-  DANCE_0,
-  TD_T_QUOT,  // KC_T, KC_QUOT
-  TD_G_EQL,   // KC_G, KC_EQL
-  TD_Y_BSLS,  // KC_Y, KC_BSLS
-  TD_H_MINS,  // KC_H, KC_MINS
-  TD_B_PLUS,  // KC_B, KC_PLUS
-  TD_N_UNDS,  // KC_N, KC_UNDS
-  TD_1_LEFT,  // KC_1, LCTL(KC_LEFT)
-  TD_GRV_RGHT,// KC_GRV, LCTL(KC_RGHT)
+    TD_LSPO,
+    TD_RSPC,
+    TD_E_LCBR,  // KC_E, KC_LCBR
+    TD_R_LBRC,  // KC_R, KC_LBRC
+    TD_U_RBRC,  // KC_U, KC_RBRC
+    TD_I_RCBR,  // KC_I, KC_RCBR
+    TD_T_QUOT,  // KC_T, KC_QUOT
+    TD_G_EQL,   // KC_G, KC_EQL
+    TD_Y_BSLS,  // KC_Y, KC_BSLS
+    TD_H_MINS,  // KC_H, KC_MINS
+    TD_B_PLUS,  // KC_B, KC_PLUS
+    TD_N_UNDS,  // KC_N, KC_UNDS
+    TD_1_LEFT,  // KC_1, LCTL(KC_LEFT)
+    TD_GRV_RGHT,// KC_GRV, LCTL(KC_RGHT)
 };
 
-static tap dance_state[1];
+// _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// define tap_dance custom functions
+// _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+td_state_t cur_dance(tap_dance_state_t *state);
 
-uint8_t dance_step(tap_dance_state_t *state);
+void lspo_finished(tap_dance_state_t *state, void *user_data);
+void lspo_reset(tap_dance_state_t *state, void *user_data);
 
-uint8_t dance_step(tap_dance_state_t *state) {
+void rspc_finished(tap_dance_state_t *state, void *user_data);
+void rspc_reset(tap_dance_state_t *state, void *user_data);
+
+static td_tap_t lspo_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+static td_tap_t rspc_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+td_state_t cur_dance(tap_dance_state_t *state) {
     if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return SINGLE_TAP;
-        else return SINGLE_HOLD;
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
     } else if (state->count == 2) {
-        if (state->interrupted) return DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return DOUBLE_HOLD;
-        else return DOUBLE_TAP;
-    }
-    return MORE_TAPS;
+        if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return TD_DOUBLE_HOLD;
+        else return TD_DOUBLE_TAP;
+    } else if (state->count == 3) {
+        if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
+        else return TD_TRIPLE_HOLD;
+    } else return TD_UNKNOWN;
 }
 
-
-void on_dance_0(tap_dance_state_t *state, void *user_data);
-void dance_0_finished(tap_dance_state_t *state, void *user_data);
-void dance_0_reset(tap_dance_state_t *state, void *user_data);
-
-void on_dance_0(tap_dance_state_t *state, void *user_data) {
-    if(state->count == 3) {
-        tap_code16(KC_EQUAL);
-        tap_code16(KC_EQUAL);
-        tap_code16(KC_EQUAL);
-    }
-    if(state->count > 3) {
-        tap_code16(KC_EQUAL);
-    }
-}
-
-void dance_0_finished(tap_dance_state_t *state, void *user_data) {
-    dance_state[0].step = dance_step(state);
-    switch (dance_state[0].step) {
-        case SINGLE_TAP: register_code16(KC_EQUAL); break;
-        case SINGLE_HOLD: register_code16(KC_ESCAPE); break;
-        case DOUBLE_TAP: register_code16(KC_EQUAL); register_code16(KC_EQUAL); break;
-        case DOUBLE_SINGLE_TAP: tap_code16(KC_EQUAL); register_code16(KC_EQUAL);
+// _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+// define tap_dance each custom function behavior
+// _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+void lspo_finished(tap_dance_state_t *state, void *user_data) {
+    lspo_tap_state.state = cur_dance(state);
+    switch (lspo_tap_state.state) {
+        case TD_SINGLE_TAP: register_code16(KC_LPRN); break; // (
+        case TD_SINGLE_HOLD: register_code16(KC_LSFT); break; // Left shift
+        case TD_DOUBLE_TAP: register_code16(KC_LBRC); break; // [
+        case TD_DOUBLE_HOLD: register_code16(KC_MEH); break; // Left Control, Shift and Alt
+        // case TD_DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code16(KC_X); break;
+        case TD_TRIPLE_TAP: register_code16(KC_LCBR); break; // {
+        case TD_TRIPLE_HOLD: register_code16(KC_HYPR); break; // Left Control, Shift and Alt, GUI
+        default: break;
     }
 }
 
-void dance_0_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(10);
-    switch (dance_state[0].step) {
-        case SINGLE_TAP: unregister_code16(KC_EQUAL); break;
-        case SINGLE_HOLD: unregister_code16(KC_ESCAPE); break;
-        case DOUBLE_TAP: unregister_code16(KC_EQUAL); break;
-        case DOUBLE_SINGLE_TAP: unregister_code16(KC_EQUAL); break;
+void lspo_reset(tap_dance_state_t *state, void *user_data) {
+    switch (lspo_tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code16(KC_LPRN); break;
+        case TD_SINGLE_HOLD: unregister_code16(KC_LSFT); break;
+        case TD_DOUBLE_TAP: unregister_code16(KC_LBRC); break;
+        case TD_DOUBLE_HOLD: unregister_code16(KC_MEH); break;
+        // case TD_DOUBLE_SINGLE_TAP: unregister_code16(KC_X); break;
+        case TD_TRIPLE_TAP: unregister_code16(KC_LCBR); break;
+        case TD_TRIPLE_HOLD: unregister_code16(KC_HYPR); break;
+        default: break;
     }
-    dance_state[0].step = 0;
+    lspo_tap_state.state = TD_NONE;
+}
+
+void rspc_finished(tap_dance_state_t *state, void *user_data) {
+    rspc_tap_state.state = cur_dance(state);
+    switch (rspc_tap_state.state) {
+        case TD_SINGLE_TAP: register_code16(KC_RPRN); break; // )
+        case TD_SINGLE_HOLD: register_code16(KC_RSFT); break; // Right Shift
+        case TD_DOUBLE_TAP: register_code16(KC_RBRC); break; // ]
+        case TD_DOUBLE_HOLD: register_code16(KC_MEH); break; // Left Control, Shift and Alt
+        // case TD_DOUBLE_SINGLE_TAP: tap_code(KC_X); register_code16(KC_X); break;
+        case TD_TRIPLE_TAP: register_code16(KC_RCBR); break; // }
+        case TD_TRIPLE_HOLD: register_code16(KC_HYPR); break; // Left Control, Shift and Alt, GUI
+        default: break;
+    }
+}
+
+void rspc_reset(tap_dance_state_t *state, void *user_data) {
+    switch (rspc_tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code16(KC_RPRN); break;
+        case TD_SINGLE_HOLD: unregister_code16(KC_RSFT); break;
+        case TD_DOUBLE_TAP: unregister_code16(KC_RBRC); break;
+        case TD_DOUBLE_HOLD: unregister_code16(KC_MEH); break;
+        // case TD_DOUBLE_SINGLE_TAP: unregister_code16(KC_X); break;
+        case TD_TRIPLE_TAP: unregister_code16(KC_RCBR); break;
+        case TD_TRIPLE_HOLD: unregister_code16(KC_HYPR); break;
+        default: break;
+    }
+    rspc_tap_state.state = TD_NONE;
 }
 
 // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 // define tap_dance actions
 // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 tap_dance_action_t tap_dance_actions[] = {
-    [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
+    [TD_LSPO] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lspo_finished, lspo_reset),
+    [TD_RSPC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rspc_finished, rspc_reset),
+    [TD_E_LCBR] = ACTION_TAP_DANCE_DOUBLE(KC_E, KC_LCBR),
+    [TD_R_LBRC] = ACTION_TAP_DANCE_DOUBLE(KC_R, KC_LBRC),
+    [TD_U_RBRC] = ACTION_TAP_DANCE_DOUBLE(KC_U, KC_RBRC),
+    [TD_I_RCBR] = ACTION_TAP_DANCE_DOUBLE(KC_I, KC_RCBR),
     [TD_T_QUOT] = ACTION_TAP_DANCE_DOUBLE(KC_T, KC_QUOT),
     [TD_G_EQL] =  ACTION_TAP_DANCE_DOUBLE(KC_G, KC_EQL),
     [TD_Y_BSLS] = ACTION_TAP_DANCE_DOUBLE(KC_Y, KC_BSLS),
