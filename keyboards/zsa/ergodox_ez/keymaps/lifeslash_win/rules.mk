b846867adb66ba4729433d70f2d8787c97a58983
again <lifeslash.mail@gmail.com>
2025-01-12 17:03:44 +0900

    update keyamp files.

--- keyboards/zsa/ergodox_ez/keymaps/lifeslash_win/rules.mk
+++ keyboards/zsa/ergodox_ez/keymaps/lifeslash_win/rules.mk
@@ -16,6 +16,7 @@ RGB_MATRIX_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+DYNAMIC_TAPPING_TERM_ENABLE = yes
 
 #RGB_MATRIX_CUSTOM_USER = yes # custom rgb lighting
 #SRC += ../default/keymap.c
